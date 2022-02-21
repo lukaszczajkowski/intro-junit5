11b319f44ff477db189dc645041fa8276eb6b1fd
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2022-02-21 19:49:17 +0100

    JUnit lifecycle demo

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,14 +1,19 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class GreetingTest {
 
     private Greeting greeting;
 
+    @BeforeAll
+    public static void beforeClass() {
+        System.out.println("Before - I am only called once");
+    }
+
     @BeforeEach
     void setUp() {
+        System.out.println("In BeforeEach");
         greeting = new Greeting();
     }
 
@@ -20,5 +25,15 @@ class GreetingTest {
     @Test
     void testHelloWorld() {
         System.out.println(greeting.helloWorld("Anna"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In AfterEach");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("After - I am only called once");
     }
 }
