fa1f4c61af1f2d1c83a11830420a55cbae8cc99f
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2022-02-21 18:26:52 +0100

    Basic initial stuff

---
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -0,0 +1,24 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GreetingTest {
+
+    private Greeting greeting;
+
+    @BeforeEach
+    void setUp() {
+        greeting = new Greeting();
+    }
+
+    @Test
+    void helloWorld() {
+        System.out.println(greeting.helloWorld());
+    }
+
+    @Test
+    void testHelloWorld() {
+        System.out.println(greeting.helloWorld("Anna"));
+    }
+}
